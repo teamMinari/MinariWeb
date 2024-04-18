6fb6bfca48ee164116db27bd7d76806105b6c3ca
0ccssu <128601631+0ccssu@users.noreply.github.com>
2024-04-18 18:13:30 +0900

    feat :: Sidebar 구현
    Sidebar

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "../pages/Login/login";
+import Login from "../pages/Login/Login";
+import Dictionary from "../pages/Dictionary/Dictionary";
+import Header from '../utils/Header/Header';
+import Sidebar from "../utils/Sidebar/Sidebar";
 
 const AppRouter = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/" element={<Sidebar />} />
             </Routes>
         </Router>
     );
