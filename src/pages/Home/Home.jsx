77fc114fea0825d0e4a9ec12e1b6475db5153b2c
joshmoon827 <joshmoon827@gmail.com>
2024-05-24 20:31:36 +0900

    AddNewsFeed
    뉴스 피드 구현 및
    간단한 서버 추가

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,27 +1,38 @@
+import React, { useState } from 'react';
 import SideBar from "../../components/Sidebar/Sidebar";
 import Header from "../../components/Header/Header";
 import * as M from "../../styles/HomeStyle/HomeStyle";
 import WordComponents from "./wordComponents";
 import { useNavigate } from "react-router-dom";
+import IngChart from "../Home/PieChart";
+import StackBars from "../Home/StackBarChart";
+import SimpleNews from "./SimpleNews";
 
 const Home = () => {
     const navigate = useNavigate();
+    const [query, setQuery] = useState("경제");
+    const [input, setInput] = useState(query);
 
     const handleGoToHome = () => {
-        navigate("/wordHome");
+        navigate("/dictionary");
     };
     const handleGoToChart = () => {
-        navigate("/wordChart");
+        navigate("/dictionary");
     };
     const handleGoToCoin = () => {
-        navigate("/wordCoin");
+        navigate("/dictionary");
     };
     const handleGoToEarth = () => {
-        navigate("/wordEarth");
+        navigate("/dictionary");
     };
     const handleGoToWallet = () => {
-        navigate("/wordWallet");
+        navigate("/dictionary");
     };
+
+    const handleSearch = () => {
+        setQuery(input);
+    };
+
     return (
     <>
       <Header />
@@ -31,41 +42,69 @@ const Home = () => {
         <M.MainContent>
           <M.CenteredContent>
             <M.RecommendWordContainer>
-              <M.pageText>오늘의 추천단어</M.pageText>
-                <M.wordSpace>
+              <M.PageText>오늘의 추천단어</M.PageText>
+                <M.WordSpace>
                     <WordComponents txt="가산금리" score={1} />
                     <WordComponents txt="또 다른 단어" score={3} />
                     <WordComponents txt="가산금리" score={1} />
                     <WordComponents txt="또 다른 단어" score={3} />
                     <WordComponents txt="가산금리" score={2} />
                     <WordComponents txt="또 다른 단어" score={3} />
-                </M.wordSpace>
+                </M.WordSpace>
             </M.RecommendWordContainer>
-            <M.menu>
-                <M.house onClick={handleGoToHome}>
-                    <M.houseImg/>
-                </M.house>
+            <M.Menu>
+                <M.House onClick={handleGoToHome}>
+                    <M.StyledHouseImg/>
+                </M.House>
                 <M.HorizontalLine/>
-                <M.chart onClick={handleGoToChart}>
-                    <M.chartImg/>
-                </M.chart>
+                <M.Chart onClick={handleGoToChart}>
+                    <M.StyledChartImg/>
+                </M.Chart>
                 <M.HorizontalLine/>
-                <M.coin onClick={handleGoToCoin}>
-                    <M.coinImg/>
-                </M.coin>
+                <M.Coin onClick={handleGoToCoin}>
+                    <M.StyledCoinImg/>
+                </M.Coin>
                 <M.HorizontalLine/>
-                <M.earth onClick={handleGoToEarth}>
-                    <M.earthImg/>
-                </M.earth>
+                <M.Earth onClick={handleGoToEarth}>
+                    <M.StyledEarthImg/>
+                </M.Earth>
                 <M.HorizontalLine/>
-                <M.wallet onClick={handleGoToWallet}>
-                    <M.walletImg/>
-                </M.wallet>
-            </M.menu>
-            <M.bannerWarp>
-                <M.banner/>
-            </M.bannerWarp>
-          </M.CenteredContent>
+                <M.Wallet onClick={handleGoToWallet}>
+                    <M.StyledWalletImg/>
+                </M.Wallet>
+            </M.Menu>
+            <M.SimpleNewsContainer>
+                <M.PageText>오늘의 경제 관련 기사</M.PageText>
+                <M.SimpleNewsCenterWrapper>
+                    <M.NewsContainer>
+                        <SimpleNews query={query} />
+                    </M.NewsContainer>
+                    
+                    <input 
+                        type="text" 
+                        placeholder="검색어를 입력하세요" 
+                        value={input} 
+                        onChange={(e) => setInput(e.target.value)}
+                    />
+                    <button onClick={handleSearch}>검색</button>
+                </M.SimpleNewsCenterWrapper>
+            </M.SimpleNewsContainer>
+            <M.BannerWarp>
+                <M.Banner/>
+            </M.BannerWarp>
+            </M.CenteredContent>
+            <M.ChartContent>
+                <M.PieChartContainer>
+                    <M.ChartPageText>학습률</M.ChartPageText>
+                    <IngChart/>
+                </M.PieChartContainer>
+                <M.ChartContainer>
+                    <M.ChartPageText>출석률</M.ChartPageText>
+                    <M.ChartCenter>
+                        <StackBars/>
+                    </M.ChartCenter>
+                </M.ChartContainer>
+            </M.ChartContent>
         </M.MainContent>
       </M.PageContent>
     </>
--- src/styles/HomeStyle/HomeStyle.js
+++ src/styles/HomeStyle/HomeStyle.js
@@ -21,38 +21,38 @@ export const GlobalStyles = createGlobalStyle`
 export const bannerStyle = css`
   object-fit: contain;
   width: 900px;
+  height: 360px;
 `;
 
-
-export const walletIconStyle = css`
+export const WalletIconStyle = css`
   object-fit: contain;
   width: 202px;  /* 아이콘 너비 */
   height: 81px;  /* 아이콘 높이 */
   max-width: 100%;
   max-height: 100%;
 `;
-export const houseIconStyle = css`
+export const HouseIconStyle = css`
   object-fit: contain;
   width: 193px;  /* 아이콘 너비 */
   height: 70px;  /* 아이콘 높이 */
   max-width: 100%;
   max-height: 100%;
 `;
-export const earthIconStyle = css`
+export const EarthIconStyle = css`
   object-fit: contain;
   width: 202px;  /* 아이콘 너비 */
   height: 70px;  /* 아이콘 높이 */
   max-width: 100%;
   max-height: 100%;
 `;
-export const chartIconStyle = css`
+export const ChartIconStyle = css`
   object-fit: contain;
   width: 193px;  /* 아이콘 너비 */
   height: 70px;  /* 아이콘 높이 */
   max-width: 100%;
   max-height: 100%;
 `;
-export const coinIconStyle = css`
+export const CoinIconStyle = css`
   object-fit: contain;
   width: 193px;  /* 아이콘 너비 */
   height: 70px;  /* 아이콘 높이 */
@@ -60,61 +60,95 @@ export const coinIconStyle = css`
   max-height: 100%;
 `;
 
-export const bannerWarp = styled.div`
-  background-color: #ffffff;
+export const BannerWarp = styled.div`
   border-radius: 10px;
   margin-top: 47px;
   align-items: center;
   justify-content: flex-start;
 `
 
-export const house = styled(Link)`
+export const House = styled(Link)`
 `
-export const chart = styled(Link)`
+export const Chart = styled(Link)`
 `
-export const coin = styled(Link)`
+export const Coin = styled(Link)`
 `
-export const earth = styled(Link)`
+export const Earth = styled(Link)`
 `
-export const wallet = styled(Link)`
+export const Wallet = styled(Link)`
+`
+export const Hi = styled.div`
+  font-size: large;
 `
 
-
-export const houseImg = styled.img.attrs({
+export const StyledHouseImg = styled.img.attrs({
   src: HouseImg,
   alt: "houseImg",
 })`
-  ${houseIconStyle}
+  ${HouseIconStyle}
 `;
 
-export const chartImg = styled.img.attrs({
+export const StyledChartImg = styled.img.attrs({
   src: ChartImg,
   alt: "chartImg",
 })`
-  ${chartIconStyle}
+  ${ChartIconStyle}
 `;
 
-export const coinImg = styled.img.attrs({
+export const StyledCoinImg = styled.img.attrs({
   src: CoinImg,
   alt: "coImg",
 })`
-  ${coinIconStyle}
+  ${CoinIconStyle}
 `;
 
-export const walletImg = styled.img.attrs({
+export const StyledWalletImg = styled.img.attrs({
   src: WalletImg,
   alt: "walletImg",
 })`
-  ${walletIconStyle}
+  ${WalletIconStyle}
 `;
 
-export const earthImg = styled.img.attrs({
+export const StyledEarthImg = styled.img.attrs({
   src: EarthImg,
   alt: "earthImg",
 })`
-  ${earthIconStyle}
+  ${EarthIconStyle}
 `;
 
+export const ChartContent = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+export const ChartPageText = styled.div`
+  font-weight: 700;
+  font-size: 23px;
+  margin-bottom: 25px;
+  margin:10px ;
+`;
+
+
+export const ChartCenter = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+
+
+export const PieChartContainer = styled.div`
+  width: 400px;
+  height: 500px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  margin-top: 47px;
+  flex-direction: column;
+  display: flex;
+  padding: 30px;
+  padding-bottom: 25px;
+`
 
 export const PageContent = styled.div`
   display: flex;
@@ -128,9 +162,8 @@ export const MainContent = styled.div`
 
 export const CenteredContent = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction:column;
   align-items: center;
-  
 `;
 
 export const RecommendWordContainer = styled.div`
@@ -141,24 +174,23 @@ export const RecommendWordContainer = styled.div`
   margin-top: 47px;
   align-items: center;
   justify-content: flex-start;
-
 `;
 
-export const pageText = styled.div`
+export const PageText = styled.div`
   font-weight: 700;
   font-size: 25px;
   margin-left: 35px;
   margin-top: 45px;
 `;
 
-export const wordSpace = styled.div`
+export const WordSpace = styled.div`
   display: flex;
   margin-left: 35px;
   flex-flow: row wrap;
   margin-top:35px;
   justify-content: center;
 `;
-export const menu = styled.div`
+export const Menu = styled.div`
   width: 900px;
   height: 140px;
   background-color: #ffffff;
@@ -169,13 +201,62 @@ export const menu = styled.div`
   justify-content: center;
   padding-bottom: 29px;
 `;
+
+export const SimpleNewsCenterWrapper = styled.div`
+  width: 800px;
+  height: 300px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  margin-top: 47px;
+  display: flex;
+  border: #d2d2d2 solid 1px;
+  margin-left: 45px;
+`;
+export const NewsContainer = styled.div`
+  width: 400px;
+  height: 300px;
+  background-color: #ffffff;
+  border-radius: 29px;
+  display: flex;
+  padding-bottom: 1px;
+  border: #d2d2d2 solid 1px;
+  overflow: scroll;
+`;
+
+
+export const SimpleNewsContainer = styled.div`
+  width: 900px;
+  height: 500px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  margin-top: 47px;
+  display: flex;
+  padding-bottom: 29px;
+  flex-direction: column;
+  justify-content: baseline;
+`;
+
+
+export const ChartContainer = styled.div`
+  width: 470px;
+  height: 500px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  margin-top: 47px;
+  margin-left: 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-bottom: 29px;
+  padding:30px
+`;
 export const HorizontalLine = styled.div`
   width: 1px;
   height: 80px;
   background-color: #D5DDFD; /* 선의 색상을 설정합니다 */
   margin: 0 -25px;
 `;
-export const banner = styled.img.attrs({
+export const Banner = styled.img.attrs({
   src: BannerImg,
   alt: "bannerImg",
 })`
